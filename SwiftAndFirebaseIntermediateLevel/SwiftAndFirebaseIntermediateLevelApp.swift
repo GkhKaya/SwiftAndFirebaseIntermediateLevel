b628ef75edf2df94d2bbf5912ecdd81aa5d70bf7
GkhKaya <gkhkaya00@gmail.com>
2023-09-08 11:24:04 +0300

    first commit

---
+++ SwiftAndFirebaseIntermediateLevel/SwiftAndFirebaseIntermediateLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAndFirebaseIntermediateLevelApp.swift
+//  SwiftAndFirebaseIntermediateLevel
+//
+//  Created by Gokhan Kaya on 6.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAndFirebaseIntermediateLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
